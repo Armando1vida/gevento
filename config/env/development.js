25c8b1cf8ddf13467bba10c854838161b459a022
ArmandoConejo <armand1live@gmail.com>
2015-01-18 19:22:28 -0500

    Config to Geventos

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
 	db: 'mongodb://localhost/sms-dev',
 	app: {
-		title: 'SMS - Development Environment'
+		title: 'GEVENTOS - Development Environment'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
