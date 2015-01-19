28912bf7f8644247eb3d49243a285b9e18e870f9
ArmandoConejo <armand1live@gmail.com>
2015-01-18 21:18:23 -0500

    Config to Geventos

--- public/modules/eventos/config/eventos.client.config.js
+++ public/modules/eventos/config/eventos.client.config.js
@@ -5,7 +5,7 @@ angular.module('eventos').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Eventos', 'eventos', 'dropdown', '/eventos(/create)?');
-		Menus.addSubMenuItem('topbar', 'eventos', 'List Eventos', 'eventos');
-		Menus.addSubMenuItem('topbar', 'eventos', 'New Evento', 'eventos/create');
+		Menus.addSubMenuItem('topbar', 'eventos', 'Listar Eventos', 'eventos');
+		Menus.addSubMenuItem('topbar', 'eventos', 'Crear Evento', 'eventos/create');
 	}
 ]);
